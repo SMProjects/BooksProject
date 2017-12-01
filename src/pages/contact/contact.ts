5dbeb957a7de58549e657b76dc9e2f93739f30ef
Lutsenko Max <makc199710@me.com>
2017-12-01 05:26:58 +0200

    Help with Git

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +7,59 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  items;
 
-  constructor(public navCtrl: NavController) {
+    constructor() {
+      this.initializeItems();
+    }
 
+    initializeItems() {
+      this.items = [
+        'V-Neck T-Shirt',
+        'Bamboo Thermal Ski Coat',
+        'Raspberry Mineral Water',
+        'Strawberry Mineral Water',
+        'Blueberry Mineral Water',
+        'Lemon-Lime Mineral Water',
+        'Peach Mineral Water',
+        'Orange Mineral Water',
+        'Multi-Vitamin (90 capsules)',
+        'Flaxseed Oil 1000 mg (90 capsules)',
+        'Magnesium 250 mg (100 tablets)',
+        'Iron 65 mg (150 caplets)',
+        'Calcium 400 IU (150 tablets)',
+        'Vitamin D3 1000 IU (100 tablets)',
+        'Vitamin A 10,000 IU (125 caplets)',
+        'Vitamin C 1000 mg (100 tablets)',
+        'Vitamin B-Complex (100 caplets)',
+        'In the Kitchen with H+ Sport',
+        'Stretchy Dance Pants',
+        'Thermal Fleece Jacket',
+        'UItra-Soft Tank Top',
+        'Grunge Skater Jeans',
+        'Unisex Thermal Vest',
+        'V-Neck Pullover',
+        'V-Neck Sweater',
+        'V-Neck T-Shirt',
+        'Polo Shirt',
+        'Skater Graphic T-Shirt',
+        'Slicker Jacket',
+        'Cross-Back Training Tank'
+      ];
+    }
+
+    getItems(ev) {
+      // Reset items back to all of the items
+      this.initializeItems();
+
+      // set val to the value of the ev target
+      var val = ev.target.value;
+
+      // if the value is an empty string don't filter the items
+      if (val && val.trim() != '') {
+        this.items = this.items.filter((item) => {
+          return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        })
+      }
+    }
   }
-
-}
---
+++ src/pages/filter-modal/filter-modal.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-filter-modal',
+  templateUrl: 'filter-modal.html'
+})
+export class FilterModalPage {
+  public femaleSelected = true;
+  public maleSelected = true;
+
+  constructor(private viewController: ViewController ,
+    public navCtrl: NavController, public navParams: NavParams) {
+      console.log(this.navParams.data);
+      this.femaleSelected = this.navParams.get("femaleSelected");
+      this.maleSelected = this.navParams.get("maleSelected");
+    }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FilterModalPage');
+  }
+
+  closeModal(){
+    let filterState = {
+      femaleSelected: this.femaleSelected,
+      maleSelected: this.maleSelected
+    };
+    this.viewController.dismiss(filterState);
+    // this.navCtrl.pop();
+  }
+
+}
